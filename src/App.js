bbbd3fd22a6145d686a87661a52cbcc8a030b94d
unknown <arturohernandezvidal.ahv@gmail.com>
2022-07-16 00:07:45 -0500

    first commint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import marked from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const initialState=`
+Paragraph
+
+**Bolded** text
+
+> Block Quote!
+
+# Head 1
+## Head 2
+
+- list item
+- list item
+- list item
+
+[Visit my Github](https://github.com/arturo-vidal)
+
+
+Inline code \`<div><p>Hi!</p></div>\`
+
+\`\`\`
+Block of code:
+let e= 1;
+let i= 2;
+let x= e + i;
+\`\`\`
+
+![Github](https://cdn-icons-png.flaticon.com/512/25/25231.png)`
+
+class App extends React.Component{
+  state={
+    text: initialState
+  }
+
+  handleChange=(e)=>{
+    this.setState({
+      text: e.target.value
+    });
+  }
+
+  render(){
+    const {text}= this.state;
+    const markdown= marked(text, {brakes: true});
+    return(
+    <div className="text-center">
+      <h1>Markdown Previewer</h1>
+      <div className="row">
+        <div className="col-md-6">
+          <h3>Enter markdown</h3>
+          <textarea id="editor" value={text} onChange={this.handleChange}></textarea>
+        </div>
+        <div id="previewdiv" className="col-md-6">
+          <h3>Output</h3>
+          <div id="preview" dangerouslySetInnerHTML={{__html: markdown}}></div>
+        </div>
+      </div>
     </div>
   );
+  }
 }
 
 export default App;
